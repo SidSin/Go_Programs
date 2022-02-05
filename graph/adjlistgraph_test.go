d977bcdb243b97581eacb42f16afb9d2fa8bfea3
Siddharth Singh <siddharthsch@gmail.com>
2022-02-06 04:29:47 +0530

    Added code to TestAdjGraphCreation01.

--- graph/adjlistgraph_test.go
+++ graph/adjlistgraph_test.go
@@ -4,4 +4,17 @@ import "testing"
 
 func TestAdjGraphCreation01(t *testing.T) {
 
+	gptr1 := MakeALGraph(3, false)
+
+	g1 := *gptr1
+
+	weight := 0
+
+	//its a triangle
+	g1.InsertEdge(CreateWeightedEdge(0, 1, weight))
+	g1.InsertEdge(CreateWeightedEdge(1, 2, weight))
+	g1.InsertEdge(CreateWeightedEdge(2, 0, weight))
+
+	gptr1.PrintEdgeSet()
+
 }
