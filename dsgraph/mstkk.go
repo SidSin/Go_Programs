10a6e56be15c21f5472eeba8c33ee25baa8423af
Siddharth Singh <siddharthsch@gmail.com>
2022-02-15 20:41:58 +0530

    Added comments to MinSpanTreeKK.

--- dsgraph/mstkk.go
+++ dsgraph/mstkk.go
@@ -4,12 +4,17 @@ import (
 	"sidsin/srcmodule/graph"
 )
 
+//Uses Kruskal's algorithm to find the minimum weight spanning tree (MST)of the given graph.
+//The graph returned is the MST in the adjacency list representation.
+//The arguemnt ALG is not modified by this function.
 func MinSpanTreeKK(ALG *graph.ADJListGraph) *graph.ADJListGraph {
 
+	//disjoint set representation of the graph pointed to by the argument ALG
 	dsg := MakeDSGraph(ALG.ALVertexCount(), ALG.IsDirected())
 
 	sortedgraph := graph.SortEdgesbyWeight(ALG)
 
+	//MST with no edges
 	mstgraph := graph.MakeALGraph(ALG.ALVertexCount(), ALG.IsDirected())
 
 	for _, e := range *sortedgraph {
@@ -22,6 +27,7 @@ func MinSpanTreeKK(ALG *graph.ADJListGraph) *graph.ADJListGraph {
 
 		if m1 != m2 {
 			Union(m1, m2)
+			//add edge to the MST
 			mstgraph.InsertEdge(e)
 		}
 	}
