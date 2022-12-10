62d7c891589551b3ad7ea832836291cdc4a96553
Siddharth Singh <siddharthsch@gmail.com>
2022-12-10 23:51:44 +0530

    Burnbooks version without channel.

--- concurrentprgms/burnbooks.go
+++ concurrentprgms/burnbooks.go
@@ -5,22 +5,73 @@ import (
 	"time"
 )
 
-type Bookshelf struct {
-	capacity int
+// V2
+// added Book struct
+
+type Book struct {
+	title  string
+	isbn   int
+	status string
 }
 
-func newBookshelf(capacity int) *Bookshelf {
-	bs := Bookshelf{capacity: capacity}
+func NewBook(title string, isbn int) *Book {
+	b := Book{title: title, isbn: isbn, status: "OnBookshelf"}
+	return &b
+}
+
+type Bookshelf struct {
+	capacity int
+	initcap  int
+	slot     []*Book
+}
+
+func NewBookshelf(capacity int) *Bookshelf {
+	bs := Bookshelf{capacity: capacity, initcap: capacity}
+
+	bs.slot = make([]*Book, capacity)
+
+	for i := 0; i < bs.capacity; i++ {
+		bs.slot[i] = NewBook(string(rune(i)), i)
+	}
 	return &bs
 }
 
-func (b *Bookshelf) unloadbook() {
+// func PrintTime() {
+// 	fmt.Println(time.Now().Format("2006-01-02 15:04:05.000000000"))
+// }
+
+func (b *Bookshelf) PrintBookShelf() {
+	fmt.Println("###########################################")
+	for i := 0; i < b.initcap; i++ {
+
+		fmt.Println(b.slot[i].isbn, b.slot[i].status)
+
+	}
+	fmt.Println("###########################################")
+}
+
+func (b *Bookshelf) unloadbook() int {
+
+	fmt.Println(" --- ", time.Now().Format("2006-01-02 15:04:05.000000000"), " --- ")
+	pickedbook := -1
 
 	if b.capacity >= 1 {
-		b.capacity = b.capacity - 1
+
+		for i := 0; i < b.initcap; i++ {
+			if b.slot[i].status == "OnBookshelf" {
+				b.slot[i].status = "Picked"
+				b.capacity = b.capacity - 1
+				fmt.Println("New capacity = ", b.capacity)
+				pickedbook = b.slot[i].isbn
+				break
+			}
+		}
 	} else {
 		fmt.Println("Bookshelf is empty.")
 	}
+
+	fmt.Println("Book Unloaded = ", pickedbook)
+	return pickedbook
 }
 
 func (b *Bookshelf) Isempty() bool {
@@ -32,33 +83,47 @@ func (b *Bookshelf) Isempty() bool {
 	}
 }
 
+//No channel for communication used
+// Two workers burn books at the same time (down to nanosecond level)
 func BookBurner(b *Bookshelf, wokernum int) {
 
 	booksburned := 0
 
-	fmt.Println("    Initial capacity = ", b.capacity, b.Isempty())
+	fmt.Println("")
+	fmt.Println("(", wokernum, ") *** Initial capacity = ", b.capacity, b.Isempty(), " *** ")
+	fmt.Println("")
 
 	for !b.Isempty() {
 
-		b.unloadbook()
-		//sleep for 1 sec to simulate transport time to incinerator
-		fmt.Println("")
-		fmt.Println("")
-		fmt.Println("    (", wokernum, ")  ------------------------------------------------")
-		fmt.Println("    (", wokernum, ") Going to Incinerator ... >>")
-		time.Sleep(1000000000)
+		booknum := b.unloadbook()
+		if booknum < 0 {
 
-		//sleep for 1 sec to simulate book load on incinerator
+			fmt.Println("(", wokernum, ")", "No more books to pick.")
+			break
 
-		//sleep for 1 sec to simulate book burning
-		booksburned = booksburned + 1
-		fmt.Println("    (", wokernum, ")  Burning Book No ", booksburned)
+		} else {
+			//sleep for 1 sec to simulate transport time to incinerator
+			fmt.Println("")
+			fmt.Println("")
+			fmt.Println("    (", wokernum, ") ------------------------------------------------")
+			fmt.Println("    (", wokernum, ")", time.Now().Format("2006-01-02 15:04:05.000000000"), "Picked Book Num ", booknum)
+			fmt.Println("    (", wokernum, ")", time.Now().Format("2006-01-02 15:04:05.000000000"), "Going to Incinerator ... >>")
+			time.Sleep(1000000000)
 
-		//sleep for 1 sec to simulate transport time from incinerator
-		//fmt.Println("    Going back to BookShelf <<... ")
+			//sleep for 1 sec to simulate book load on incinerator
+
+			//sleep for 1 sec to simulate book burning
+			booksburned = booksburned + 1
+			fmt.Println("    (", wokernum, ")", time.Now().Format("2006-01-02 15:04:05.000000000"), "Burning Book No ", booknum)
+			b.slot[booknum].status = "Burned"
+
+			//sleep for 1 sec to simulate transport time from incinerator
+			fmt.Println("    (", wokernum, ") Going back to BookShelf <<... ")
+			time.Sleep(1000000000)
+		}
 	}
 
 	fmt.Println("")
 	fmt.Println("")
-	fmt.Println("(", wokernum, ")    No of books burned = ", booksburned)
+	fmt.Println("(", wokernum, ") No of books burned = ", booksburned)
 }
--- concurrentprgms/burnbooks_test.go
+++ concurrentprgms/burnbooks_test.go
@@ -8,16 +8,33 @@ import (
 
 func TestBookBurning(t *testing.T) {
 
-	bs1 := newBookshelf(7)
+	initcapacity := 6
+
+	bs1 := NewBookshelf(initcapacity)
 
 	fmt.Println("")
 	fmt.Println("")
-	fmt.Println("    bs1 capcity = ", bs1.capacity, bs1.Isempty())
+
+	bs1.PrintBookShelf()
+
+	//fmt.Println("    bs1 capcity = ", bs1.capacity, bs1.Isempty())
 
 	go BookBurner(bs1, 1)
 	go BookBurner(bs1, 2)
+	// go BookBurner(bs1, 3)
+	// go BookBurner(bs1, 4)
+	// go BookBurner(bs1, 5)
+	// go BookBurner(bs1, 6)
+	// go BookBurner(bs1, 7)
+	// go BookBurner(bs1, 8)
+	// go BookBurner(bs1, 9)
+	// go BookBurner(bs1, 10)
 
-	time.Sleep(1000000000 * 20)
+	time.Sleep(1000000000 * 10)
+
+	fmt.Println("     Final Capacity = ", bs1.capacity)
+	bs1.PrintBookShelf()
+
 	fmt.Println("    Ending TestBookBurning")
 
 }
